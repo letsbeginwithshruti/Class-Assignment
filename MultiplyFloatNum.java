c7b90c7a2b630e2e0067a60ac8a1a46cfb57bccf
Shruti <114383443+letsbeginwithshruti@users.noreply.github.com>
2022-12-23 18:35:15 +0530

    Create MultiplyFloatNum.java

---
+++ MultiplyFloatNum.java
@@ -0,0 +1,10 @@
+//Write a java program to multiply two floating-point numbers
+public class MultiplyFloatNum
+{
+public static void main(String args[])
+{
+float a=7.8,b=5.5,product;
+product=a*b;
+System.out.println("The Product is:- "+product);
+}
+}
