84aba8c73c034b869507aa17d768aa053cfe5d70
Shruti <114383443+letsbeginwithshruti@users.noreply.github.com>
2022-12-23 18:32:23 +0530

    Create Add.java

---
+++ Add.java
@@ -0,0 +1,10 @@
+//Write a java program to add two numbers
+public class Add
+{
+public static void main(String args[])
+{
+int a=624,b=166,sum;
+sum=a+b;
+System.out.println("The sum is:- "+sum);
+}
+}
